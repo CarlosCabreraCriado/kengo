82c5a182061ddb5220d496d7ef3fe3633403aca3
Plumbico <emilioopb96@gmail.com>
2025-02-20 13:32:30 +0000

    Commit Inicio 1 parte

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
+import { EjerciciosComponent } from './ejercicios/ejercicios.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },
   { path: 'inicio', component: InicioComponent, pathMatch: 'full' },
+  { path: 'ejercicios', component: EjerciciosComponent, pathMatch: 'full' },
 ];
