6343b45074d23e548b62e25eab0754081cbb79d3
Plumbico <emilioopb96@gmail.com>
2025-02-24 17:31:16 +0000

    commit ejercicios y navegacion

---
+++ src/app/fisios/fisios.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fisios',
+  imports: [],
+  templateUrl: './fisios.component.html',
+  styleUrl: './fisios.component.css'
+})
+export class FisiosComponent {
+
+}
