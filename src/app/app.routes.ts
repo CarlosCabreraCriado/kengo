8a1fb396d284828dbed20914b08d2c2a806714ae
Plumbico <emilioopb96@gmail.com>
2025-02-25 13:25:22 +0000

    commit routerlinks y fisios incompleto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { EjerciciosComponent } from './ejercicios/ejercicios.component';
+import { FisiosComponent } from './fisios/fisios.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },
   { path: 'inicio', component: InicioComponent, pathMatch: 'full' },
   { path: 'ejercicios', component: EjerciciosComponent, pathMatch: 'full' },
+  { path: 'fisios', component: FisiosComponent, pathMatch: 'full' },
 ];
---
+++ src/app/detalle-ejercicio/detalle-ejercicio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle-ejercicio',
+  imports: [],
+  templateUrl: './detalle-ejercicio.component.html',
+  styleUrl: './detalle-ejercicio.component.css'
+})
+export class DetalleEjercicioComponent {
+
+}
