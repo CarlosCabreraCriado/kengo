916719d33395829efccf26ca9777f46cbde6670a
Plumbico <emilioopb96@gmail.com>
2025-02-26 13:01:16 +0000

    commit categorias 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { EjerciciosComponent } from './ejercicios/ejercicios.component';
 import { FisiosComponent } from './fisios/fisios.component';
+import { CategoriasComponent } from './categorias/categorias.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },
   { path: 'inicio', component: InicioComponent, pathMatch: 'full' },
   { path: 'ejercicios', component: EjerciciosComponent, pathMatch: 'full' },
   { path: 'fisios', component: FisiosComponent, pathMatch: 'full' },
+  { path: 'categorias', component: CategoriasComponent, pathMatch: 'full' },
 ];
--- src/app/categorias/categorias.component.ts
+++ src/app/categorias/categorias.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { NavegacionComponent } from '../navegacion/navegacion.component';
 
 @Component({
   selector: 'app-categorias',
-  imports: [],
+  imports: [NavegacionComponent],
   templateUrl: './categorias.component.html',
-  styleUrl: './categorias.component.css'
+  styleUrl: './categorias.component.css',
 })
-export class CategoriasComponent {
-
-}
+export class CategoriasComponent {}
