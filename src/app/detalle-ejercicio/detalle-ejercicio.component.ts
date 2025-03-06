dde04d0abf401f70cd5b09c5b1d5eaeee45e3742
Plumbico <emilioopb96@gmail.com>
2025-03-06 11:54:10 +0000

    pre-clientes

--- src/app/detalle-ejercicio/detalle-ejercicio.component.ts
+++ src/app/detalle-ejercicio/detalle-ejercicio.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavegacionComponent } from '../navegacion/navegacion.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-detalle-ejercicio',
-  imports: [NavegacionComponent],
+  imports: [NavegacionComponent, RouterLink],
   templateUrl: './detalle-ejercicio.component.html',
   styleUrl: './detalle-ejercicio.component.css',
 })
--- src/app/ejercicios/ejercicios.component.ts
+++ src/app/ejercicios/ejercicios.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavegacionComponent } from '../navegacion/navegacion.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-ejercicios',
-  imports: [NavegacionComponent],
+  imports: [NavegacionComponent, RouterLink],
   templateUrl: './ejercicios.component.html',
   styleUrl: './ejercicios.component.css',
 })
