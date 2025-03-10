075f4b4febce2c9ee30652782d5d8ec53212a4e5
Plumbico <emilioopb96@gmail.com>
2025-03-10 15:07:09 +0000

    mis clientes

---
+++ src/app/perfil/perfil.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-perfil',
+  imports: [RouterLink],
+  templateUrl: './perfil.component.html',
+  styleUrl: './perfil.component.css',
+})
+export class PerfilComponent {}
