c71a5e09ff3594547ec219fc340e842b250ba9c9
Plumbico <emilioopb96@gmail.com>
2025-03-12 13:17:44 +0000

    login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,8 @@ import { DetalleEjercicioComponent } from './detalle-ejercicio/detalle-ejercicio
 import { ClientesComponent } from './clientes/clientes.component';
 import { PerfilComponent } from './perfil/perfil.component';
 import { MiclinicaComponent } from './miclinica/miclinica.component';
+import { ClientePerfilComponent } from './cliente-perfil/cliente-perfil.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },
@@ -22,4 +24,10 @@ export const routes: Routes = [
   { path: 'clientes', component: ClientesComponent, pathMatch: 'full' },
   { path: 'perfil', component: PerfilComponent, pathMatch: 'full' },
   { path: 'miclinica', component: MiclinicaComponent, pathMatch: 'full' },
+  {
+    path: 'cliente-perfil',
+    component: ClientePerfilComponent,
+    pathMatch: 'full',
+  },
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
 ];
---
+++ src/app/cliente-perfil/cliente-perfil.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cliente-perfil',
+  imports: [],
+  templateUrl: './cliente-perfil.component.html',
+  styleUrl: './cliente-perfil.component.css'
+})
+export class ClientePerfilComponent {
+
+}
