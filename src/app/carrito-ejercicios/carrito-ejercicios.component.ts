b7a77a35335e45bfc2d5b07188dfdb259182a7c7
Plumbico <emilioopb96@gmail.com>
2025-04-15 08:29:43 +0100

    crear carrito ejercicioos

---
+++ src/app/carrito-ejercicios/carrito-ejercicios.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrito-ejercicios',
+  imports: [],
+  templateUrl: './carrito-ejercicios.component.html',
+  styleUrl: './carrito-ejercicios.component.css'
+})
+export class CarritoEjerciciosComponent {
+
+}
